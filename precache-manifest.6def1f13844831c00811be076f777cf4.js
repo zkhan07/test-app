edf8c110c55cc0b82214671c802ed4218cf88311
zaid <khanzaid786786.kz@gmail.com>
2021-01-22 16:39:51 +0530

    Updates

---
+++ precache-manifest.6def1f13844831c00811be076f777cf4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ffe576ad2cdf28b2c5e",
+    "url": "/test-app/static/js/0.17a97926.chunk.js"
+  },
+  {
+    "revision": "963decb3efbdecb704b0",
+    "url": "/test-app/static/css/main.cb27d8ee.chunk.css"
+  },
+  {
+    "revision": "963decb3efbdecb704b0",
+    "url": "/test-app/static/js/main.99dbe6b3.chunk.js"
+  },
+  {
+    "revision": "f5571fc7724c30a54ea8",
+    "url": "/test-app/static/js/runtime~main.cb64688c.js"
+  },
+  {
+    "revision": "7c73d969f2968ffcafda",
+    "url": "/test-app/static/js/3.3f15c1b1.chunk.js"
+  },
+  {
+    "revision": "caa7d6d6365fac4659f8",
+    "url": "/test-app/static/js/4.d42403ac.chunk.js"
+  },
+  {
+    "revision": "e86eb18a70c5d409285c",
+    "url": "/test-app/static/js/5.2ed55963.chunk.js"
+  },
+  {
+    "revision": "86c90d31483e0bc04f28",
+    "url": "/test-app/static/css/6.b3525f8f.chunk.css"
+  },
+  {
+    "revision": "86c90d31483e0bc04f28",
+    "url": "/test-app/static/js/6.2ef06d48.chunk.js"
+  },
+  {
+    "revision": "a1a985649e6c4928c097",
+    "url": "/test-app/static/js/7.78e94481.chunk.js"
+  },
+  {
+    "revision": "b93a94de1e17a11b269db95f08f3410b",
+    "url": "/test-app/static/media/bg2.b93a94de.png"
+  },
+  {
+    "revision": "0d65954195b8daee21d2d83d47ec3b37",
+    "url": "/test-app/index.html"
+  }
+];
